b1a67c9981acc54ac873ef8041a0daaccd33e077
ben-farrelly <benjamin.farrelly@serato.com>
2019-10-02 12:52:28 +1300

    Adding bearer auth in the request build to ensure new access tokens can be used

--- sws_py_sdk/service.py
+++ sws_py_sdk/service.py
@@ -103,7 +103,7 @@ class Service(object):
         request = Request(method=method, url=endpoint, headers=headers)
 
         if auth is 'bearer':
-            request = self.sws.bearer_auth(request)
+            request.headers['Authorization'] = "Bearer %s" % self.sws.access_token
         elif isinstance(auth, HTTPBasicAuth):
             request.auth = auth
 
