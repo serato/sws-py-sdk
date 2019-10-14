4c0e3bd25c6666dbfc960040d05fc2a9027624fc
Takayuki Ibaraki <takayuki.ibaraki@serato.com>
2019-10-14 13:28:19 +1300

    Fix tests broken during merge
    - Tests merged to master still assumed the behaviour of the request function

--- tests/integration/identity_test.py
+++ tests/integration/identity_test.py
@@ -20,28 +20,30 @@ def test_login(me_endpoint_sws_client):
     assert response.status_code != 404
     assert response.status_code != 500
 
+
 def test_create_user(me_endpoint_sws_client):
     response = me_endpoint_sws_client.identity().post_users(
         email_address="test.useruser@serato.com",
         password="test_password"
     )
 
-    assert error_response.response.status_code != 404
-    assert error_response.response.status_code != 500
+    assert response.status_code != 404
+    assert response.status_code != 500
+
 
 def test_me_groups(me_endpoint_sws_client):
-    error_response = me_endpoint_sws_client.identity().post_groups(
+    response = me_endpoint_sws_client.identity().post_groups(
         group_name="serato"
     )
 
-    assert error_response.response.status_code != 404
-    assert error_response.response.status_code != 500
+    assert response.status_code != 404
+    assert response.status_code != 500
 
 
 def test_user_groups(user_endpoint_sws_client):
-    error_response = user_endpoint_sws_client.identity().post_groups(
+    response = user_endpoint_sws_client.identity().post_groups(
         group_name="serato"
     )
 
-    assert error_response.response.status_code != 404
-    assert error_response.response.status_code != 500
+    assert response.status_code != 404
+    assert response.status_code != 500
