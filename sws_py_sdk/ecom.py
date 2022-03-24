bd57b6afae40f74364c614efd78364b8f86bc58b
Gabriel <youngmin.jo@serato.com>
2022-03-24 13:04:27 +1300

    Add promotion endpoints

--- sws_py_sdk/ecom.py
+++ sws_py_sdk/ecom.py
@@ -1,6 +1,7 @@
 """ This file exposes endpoints from the SWS Ecom Service
 """
 
+from contextlib import nullcontext
 from requests.auth import HTTPBasicAuth
 
 from sws_py_sdk.service import Service
@@ -207,4 +208,119 @@ class Ecom(Service):
             body={"notification_kind": notification_kind, "subscription_id": subscription_id},
             endpoint=endpoint,
             method="POST"
+        )
+
+    def create_promotion(self, name, description, coupon_based, enabled, starts_at=None, ends_at=None):
+        """ Create a promotion
+
+            name: string
+            description: string
+            starts_at: string
+                datetime format
+            ends_at: string
+                datetime format
+            coupon_based: boolean
+            endabled: boolean
+        """
+        endpoint = '/api/v1/promotions'
+        return self.fetch(
+            auth=HTTPBasicAuth(username=self.sws.app_id, password=self.sws.secret),
+            body={
+                "name": name,
+                "description": description,
+                "starts_at": starts_at,
+                "ends_at": ends_at,
+                "coupon_based": coupon_based,
+                "enabled": enabled
+            },
+            endpoint=endpoint,
+            method="POST"
+        )
+
+    def create_promotion_coupons(
+        self,
+        promotion_id,
+        usage_limit=None,
+        usage_limit_per_user=None,
+        expires_at=None,
+        coupon_code=None
+    ):
+        """ Create a promotion coupon
+
+            promotion_id: int
+            usage_limit: int
+            usage_limit_per_user: int
+            expires_at: string
+                datetime format
+            coupon_code: string
+        """
+        endpoint = f"/api/v1/promotions/{promotion_id}/coupons"
+        return self.fetch(
+            auth=HTTPBasicAuth(username=self.sws.app_id, password=self.sws.secret),
+            body={
+                "usage_limit": usage_limit,
+                "usage_limit_per_user": usage_limit_per_user,
+                "expires_at": expires_at,
+                "coupon_code": coupon_code
+            },
+            endpoint=endpoint,
+            method="POST"
+        )
+
+    def create_promotion_rule(
+        self,
+        promotion_id,
+        product_type_id,
+        discount_percentage,
+        discount_fixed_amount,
+        pre_condition_product_id,
+        braintree_discount_id,
+        subscription_promotion_expires
+    ):
+        """ Create a promotion rule
+            promotion_id: int
+            product_type_id: int
+            discount_percentage: float
+            discount_fixed_amount: float
+            pre_condition_product_id: int
+            braintree_discount_id: string
+            subscription_promotion_expires: int
+        """
+        endpoint = f"/api/v1/promotions/{promotion_id}/rules"
+        return self.fetch(
+            auth=HTTPBasicAuth(username=self.sws.app_id, password=self.sws.secret),
+            body={
+                "product_type_id": product_type_id,
+                "discount_percentage": discount_percentage,
+                "discount_fixed_amount": discount_fixed_amount,
+                "pre_condition_product_id": pre_condition_product_id,
+                "braintree_discount_id": braintree_discount_id,
+                "subscription_promotion_expires": subscription_promotion_expires
+            },
+            endpoint=endpoint,
+            method="POST"
+        )
+
+    def delete_promotion(self, promotion_id):
+        """ Deletes a promotion
+
+            promotion_id: int
+        """
+        endpoint = f"/api/v1/promotions/{promotion_id}"
+        return self.fetch(
+            auth=HTTPBasicAuth(username=self.sws.app_id, password=self.sws.secret),
+            endpoint=endpoint,
+            method="DELETE"
+        )
+    
+    def delete_promotion_coupon(self, promotion_id, coupon_code):
+        """ Deletes a promotion coupon
+
+            promotion_id: int
+        """
+        endpoint = f"/api/v1/promotions/{promotion_id}/coupons/{coupon_code}"
+        return self.fetch(
+            auth=HTTPBasicAuth(username=self.sws.app_id, password=self.sws.secret),
+            endpoint=endpoint,
+            method="DELETE"
         )
